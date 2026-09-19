171a5eebd83e4fb80f7b4198c8bbab0ae71a3b41
KhengLyratanak <khenglyratanak.gmail.com>
2026-09-19 22:03:30 +0700

    refactor(user): implement email user for login

--- src/main/java/com/nak/demo/repository/UserRepository.java
+++ src/main/java/com/nak/demo/repository/UserRepository.java
@@ -20,4 +20,5 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Boolean existsByEmail(String email);
 
     Optional<User> findByName(String usename);
+    Optional<User> findByEmail(String email);
 }
